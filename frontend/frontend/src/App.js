0b40956e531baec80d2c5339333c5a3ba364c9e1
Jincy Jeevan <2000ashnajeevan@gmail.com>
2024-04-20 10:54:58 +0530

    studentlogin added

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import StudentDash from './components/StudentDash';
 import { Routes, Route } from 'react-router-dom';
+import StudentLogin from './components/StudentLogin';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path={'/sdash'} element={<StudentDash/>}/>
+        <Route path={'/studentlog'} element={<StudentLogin/>}/>
       </Routes>
     </div>
   );
