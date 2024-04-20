50c11b317ca42bf159bf5db1aa081644e659db3f
judeClement <judeclement71@gmail.com.com>
2024-04-20 13:58:03 +0530

    new commit

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -4,11 +4,15 @@ import StudentDash from './components/StudentDash';
 import AdminLogin from './components/AdminLogin';
 import { Routes, Route } from 'react-router-dom';
 import StudentLogin from './components/StudentLogin';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
+      <Route path={'/home'} element={<Home />} />
         <Route path={'/sdash'} element={<StudentDash/>}/>
 <Route path='/adminlog' element={<AdminLogin/>}/>
 <Route path={'/studentlog'} element={<StudentLogin/>}/>
@@ -19,3 +23,5 @@ function App() {
 }
 
 export default App;
+
+
