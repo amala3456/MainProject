83b56271c4f19d99ae945969aabb17dc1518476b
amala3456 <amalajames1995@gmail.com>
2024-04-27 01:56:07 +0530

    final commit

--- backend/app.js
+++ backend/app.js
@@ -2,17 +2,16 @@ const express = require('express');
 const morgan = require('morgan');
 const cors=require('cors')
 require('dotenv').config();
-const cors = require('cors');
-
 const app = new express();
 const PORT = process.env.PORT ;
 app.use(morgan('dev'));
-
 
 const studentlogRouter = require('./Routes/studentlogRoutes');
 const studentlogDb = require('./Db/studentlogDb');
 const sdashDb=require('./Db/sdashDb');
 const sdashRouter=require('./Routes/sdashRoutes');
+app.use(cors());
+
 
 app.use('/api',sdashRouter)
 
