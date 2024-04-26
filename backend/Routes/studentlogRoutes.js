8ac108ce5bf4b9f98a36d6e41b91b400293a6060
Jincy Jeevan <2000ashnajeevan@gmail.com>
2024-04-27 00:33:20 +0530

    student log backend

--- backend/Routes/studentlogRoutes.js
+++ backend/Routes/studentlogRoutes.js
@@ -1,4 +1,30 @@
 const express = require('express');
 const studentlogRouter = express.Router();
+const studentlogSchema = require('../Model/studentSchema');
+
+studentlogRouter.use(express.json());
+
+
+// dummy email and password
+const Email = 'johndoe@gmail.com';
+const Password = 'John@123';
+
+// Login route
+studentlogRouter.post('/studentlog', async (req, res) => {
+    let email = req.body.email;
+    let password = req.body.password;
+
+    try {
+        if (email !== Email || password !== Password) {
+            res.status(401).json({ message: "Incorrect email or password" });
+            return;
+        }
+        
+        res.status(200).json({ message: "Login successful" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+});
 
 module.exports = studentlogRouter;
--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
 require('dotenv').config();
+const cors = require('cors');
+
+
 
 const studentlogRouter = require('./Routes/studentlogRoutes');
 const studentlogDb = require('./Db/studentlogDb');
@@ -8,6 +11,9 @@ const studentlogDb = require('./Db/studentlogDb');
 const app = new express();
 const PORT = process.env.PORT ;
 app.use(morgan('dev'));
+
+app.use(cors());
+app.use('/api',studentlogRouter);
 
 app.listen(PORT,()=>{
     console.log(`Server is up and running on port ${PORT}`);
